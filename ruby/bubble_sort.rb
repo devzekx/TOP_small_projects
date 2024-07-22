5b78c9d348a6930caf220d0f364c05c0e84e344b
zenok <dev.zekx@gmail.com>
2024-07-22 22:17:14 +0200

    completed bubble_sort.rb, adjusted comments in substring.rb

--- ruby/bubble_sort.rb
+++ ruby/bubble_sort.rb
@@ -1,20 +1,18 @@
-class Array
-  def bubble_sort
-    n = self.length # find the full length from the array - self talks about instance of the class (array example)
+  def bubble_sort(arr)
+    n = arr.length # find the full length from the array
     loop do
       swapped = false # sentinel value
       (n-1).times do |i| # nested loop; iterate to the last integer in array
-        if self[i] > self[i+1]
-          self[i], self[i+1] = self[i+1], self[i]
+        if arr[i] > arr[i+1]
+          arr[i], arr[i+1] = arr[i+1], arr[i]
           swapped = true
         end
       end
       break if not swapped # sentinel value -> breaks the loop
     end
-    self
+    arr
   end
-end
 
 arr = [4, 6, 2, 3, 5, 1, 7]
 
-p arr.bubble_sort
+p bubble_sort(arr)

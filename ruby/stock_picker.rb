06d7dd7f03142e4baa55c6631589296bd7ca6f29
zenok <dev.zekx@gmail.com>
2024-07-24 08:29:39 +0200

    started stock_picker.rb

--- ruby/stock_picker.rb
+++ ruby/stock_picker.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-def stock_picker(arr)
-  min_index = 0
-  max_index = 1
-  max_profit = prices[max_index] - prices[min_index]
-  
-  prices.each_with_index do |price, index|
-    if price < prices[min_index]
-      min_index = index
-    end
-  end
+def stock_picker(prices)
+  (0...prices.size).to_a.combination(2).max_by { |buy, sell| prices[sell] - prices[buy] }
 end
-stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
+
+# (0...prices.size.to_a) => creates an array of all day indices
+# combination(2) => generates all possible pairs of days
+# max_by => finds pair of days that result in max profit by subtracting buy day price from sell day price: prices[sell] - prices[buy]
+# we put prices[sell] first => biggest amount we can sell
+# we put prices[buy] last => lowest amount to buy
